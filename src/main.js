32c064991caf1e81be9861d11a2b61f069941a19
Nola <sartova.nadia@yandex.ru>
2022-10-04 23:01:31 +0700

    1111

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+// import IndexPage from '@/views/IndexPage.vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
---
+++ src/views/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="themes__wrapper">
+    <h1 class="themes__title">
+      Оказание первой помощи. Практическая часть
+    </h1>
+    <h2 class="themes__title">Выбирете тему:</h2>
+    <ol class="themes-list">
+      <li class="themes-list__item">Оценка обстановки на месте происшествия</li>
+      <li class="themes-list__item">Отработка навыков определения сознания у пострадавшего</li>
+      <li class="themes-list__item">Отработка приемов восстановления проходимости верхних дыхательных путей.
+        Оценка признаков жизни у пострадавшего</li>
+      <li class="themes-list__item">Отработка вызова скорой медицинской помощи, других специальных служб</li>
+      <li >Отработка приемов искусственного дыхания "рот ко рту",
+        "рот к носу" с применением устройств для искусственного дыхания</li>
+      <li class="themes-list__item">Отработка приемов давления руками на грудину пострадавшего</li>
+      <li class="themes-list__item">Выполнение алгоритма реанимации</li>
+      <li class="themes-list__item">Отработка приема перевода пострадавшего в устойчивое боковое положение</li>
+      <li class="themes-list__item">Отработка приемов удаления инородного тела из верхних
+        дыхательных путей пострадавшего</li>
+    </ol>
+    <button>Начать</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped lang="sass">
+.themes__wrapper
+  margin: 50px
+.themes__title
+  margin-bottom: 30px
+
+.themes-list__item
+  padding: 20px
+</style>
