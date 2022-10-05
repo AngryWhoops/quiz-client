69a7d620f35c37402cf26e54ab18d96470105904
Nola <sartova.nadia@yandex.ru>
2022-10-05 20:08:09 +0700

    222

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -2,31 +2,87 @@
   <div class="themes__wrapper">
     <h1 class="themes__title">
       Оказание первой помощи. Практическая часть
-    </h1>
-    <ol class="themes-list">
-      <h2 class="themes__title">Выбирете тему:</h2>
-      <li class="themes-list__item">Оценка обстановки на месте происшествия</li>
-      <li class="themes-list__item">Отработка навыков определения сознания у пострадавшего</li>
-      <li class="themes-list__item">Отработка приемов восстановления проходимости верхних дыхательных путей.
-        Оценка признаков жизни у пострадавшего</li>
-      <li class="themes-list__item">Отработка вызова скорой медицинской помощи, других специальных служб</li>
-      <li class="themes-list__item">Отработка приемов искусственного дыхания "рот ко рту",
-        "рот к носу" с применением устройств для искусственного дыхания</li>
-      <li class="themes-list__item">Отработка приемов давления руками на грудину пострадавшего</li>
-      <li class="themes-list__item">Выполнение алгоритма реанимации</li>
-      <li class="themes-list__item">Отработка приема перевода пострадавшего в устойчивое боковое положение</li>
-      <li class="themes-list__item">Отработка приемов удаления инородного тела из верхних
-        дыхательных путей пострадавшего</li>
-    </ol>
-    <button class="btn">Начать</button>
+    </h1>    
+    <h2 class="themes__title">Выбирете тему:</h2>
+
+    <div 
+      class="themes-list"
+      v-for="theme in themes"
+      v-bind:key="theme"
+      v-bind:theme="theme">
+      <label class="label">
+        <input 
+          class="input" 
+          type="radio" 
+          name="theme" 
+          v-model="inputValue"
+          :value="theme.value">{{theme.label}}
+      </label>
+    </div>
+    
+    <div class="btn__wrapper">
+      <button 
+        class="btn"
+        @click="isThemeChecked"
+      >Начать
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
   data() {
     return {
-    };
+      themes: [
+        {
+          value: 'situation',
+          label: 'Оценка обстановки на месте происшествия',
+        },
+        {
+          value: 'consciousness',
+          label: 'Отработка навыков определения сознания у пострадавшего',
+        },
+        {
+          value: 'life',
+          label: 'Отработка приемов восстановления проходимости верхних дыхательных путей. Оценка признаков жизни у пострадавшего',
+        },
+        {
+          value: 'ambulance',
+          label: 'Отработка вызова скорой медицинской помощи, других специальных служб',
+        },
+        {
+          value: 'artificial_respiration',
+          label: 'Отработка приемов искусственного дыхания "рот ко рту", "рот к носу" с применением устройств для искусственного дыхания',
+        },
+        {
+          value: 'sternum',
+          label: 'Отработка приемов давления руками на грудину пострадавшего',
+        },
+        {
+          value: 'resuscitation',
+          label: 'Выполнение алгоритма реанимации',
+        },
+        {
+          value: 'side_pose',
+          label: 'Отработка приема перевода пострадавшего в устойчивое боковое положение',
+        },
+        {
+          value: 'subject',
+          label: 'Отработка приемов удаления инородного тела из верхних дыхательных путей пострадавшего',
+        },
+      ],
+      inputValue: '',
+    }   
+  },
+  methods: {
+    isThemeChecked() {      
+      if(!this.inputValue) {        
+        alert('Тема не выбрана');        
+      }      
+    },
   },
 };
 </script>
@@ -40,21 +96,35 @@ export default {
 .themes-list
   width: 70%
   margin: 0 auto
-  &__item    
-    padding: 20px
-    background: #fcb291
-    margin-bottom: 10px
-    border-radius: 10px
-    font-size: 20px
-    cursor: pointer
-    transition: 0.3s all
-    &:hover
-      transform: scale(1.1)
+  list-style-type: none
+.label
+  width: 100%
+  display: inline-block
+  padding: 10px 20px
+  cursor: pointer
+  background: #fcb291
+  margin-bottom: 10px
+  border-radius: 10px
+  font-size: 20px    
+  transition: 0.3s all
+  &:hover
+    transform: scale(1.05)
+.input
+  margin-right: 10px
+  cursor: pointer
+input[type='radio']
+  color: red
+.btn__wrapper
+  display: flex
+  justify-content: center
 .btn
-  padding: 20px
+  padding: 20px 60px
   background: #fcb291
   border: none
   border-radius: 10px
   font-size: 25px
   cursor: pointer
+.text
+  display: none
 </style>
+
