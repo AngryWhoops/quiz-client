df199e9fc24639caa82b577981cd86ccf0a59625
Danil <kuznetsovdanil.dka@gmail.com>
2022-10-22 03:45:27 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,14 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //base: process.env.NODE_ENV === "production" ? "/quiz-client/" : "./",
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath: "/",
+  base: "/quiz-cient/",
 });
 
 // const { defineConfig } = require('@vue/cli-service');
