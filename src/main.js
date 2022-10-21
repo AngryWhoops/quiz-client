5d6065a0be6c907f5f20a05db8ac9a5807c17a70
Danil <kuznetsovdanil.dka@gmail.com>
2022-10-22 01:59:52 +0700

    changed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue';
-// import IndexPage from '@/views/IndexPage.vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import "@/assets/css/fonts.css"
+import "./assets/main.css";
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
