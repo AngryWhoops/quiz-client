a94d79a796eb0df223d5da156741773aea453a84
Danil <kuznetsovdanil.dka@gmail.com>
2022-10-22 11:33:05 +0700

    .

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-// import IndexPage from '@/views/IndexPage.vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-
-import "@/assets/css/fonts.css"
-
-createApp(App).use(store).use(router).mount('#app');
