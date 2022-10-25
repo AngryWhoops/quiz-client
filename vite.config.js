6858b7ba13cc4b241bf9a3cb04fd3ec382750495
Nola <sartova.nadia@yandex.ru>
2022-10-25 14:17:32 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,18 @@
 import vue from "@vitejs/plugin-vue";
-// import { fileURLToPath, URL } from "node:url";
-import { defineConfig, process } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 //https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       // "@": fileURLToPath(new URL("./src", import.meta.url)),
-//       "@": fileURLToPath(new URL("./src", import.meta.url)),
-//     },
-//   },
-//   base: "/quiz-client/",
-//   //base: new URL(import.meta.env.VITE_URL),
-// });
-
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   // base: "/quiz-client/",
   base: new URL(import.meta.env.VITE_URL),
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production",
 });
 
 // export default defineConfig(({ command }) => {
